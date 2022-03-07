16c4d8ba28976e4755fee6cfa41c91992018d23e
Reynan Kanindot <ekahiyang@gmail.com>
2022-03-07 20:46:32 +1100

    updated seeds.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
-
+const postData = require('./postData.json');
+const commentData = require('./commentsData.json')
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
