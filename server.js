7860076700f5e16a6c9d0eed9ba2781f5a013307
Reynan Kanindot <ekahiyang@gmail.com>
2022-03-07 16:55:54 +1100

    added helmet library for express security

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const helmet = require("helmet"); //added to secure express app
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -8,8 +9,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+require('dotenv').config(); //Added so that secret key will not be visible for accessing
+app.use(helmet()); //invoke helmet function for security
+
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.secret, //secret key will refer to .env file.
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -20,11 +24,17 @@ const sess = {
 
 app.use(session(sess));
 
+
+// Informs Express.js to use the handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port localhost:${PORT}`));
 });
